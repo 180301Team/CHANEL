1eed4c5ffa82d9cc8fabfa3bf04d62b52e1299fe
DevinSun036 <634784822@qq.com>
2018-06-15 16:08:47 +0800

    add homepage

---
+++ js/homepage.js
@@ -0,0 +1,13 @@
+// $('.mybanner').mouseenter(function(){
+//     $('.mask').fadeIn(500);
+//     // $('.mask').find("a").addClass("animated bounceInLeft");
+// });
+// $('.mybanner').mouseleave(function(){
+//     $('.mask').fadeOut(500);
+// });
+// $('.mack ul li').mouseenter(function(){
+//     $(this).find('a').css('font-size','30px');
+// });
+// $('.mack ul li').mouseleave(function(){
+//     $(this).find('a').css('font-size','20px');
+// });
